d12802183120af56c956491e465d763eb8bc9d29
Villiruusu <javillir@gmail.com>
2019-10-09 21:35:48 +0300

    Poistettu kovakoodatut tiedost persons-taulukosta

--- Osa 2/puhelinluettelo/src/App.js
+++ Osa 2/puhelinluettelo/src/App.js
@@ -4,12 +4,7 @@ import Persons from './components/Persons.js'
 import Filter from './components/Filter.js'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567'},
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
